1c37cd520a5b92c54d6b5a369c8249b00c520c58
Peng Wang <peng@pengxwang.com>
2017-05-21 14:16:44 -0700

    ES6 rewrite: port test/base-visual into test/base.

--- tests/js/base.js
+++ tests/js/base.js
@@ -26,6 +26,113 @@ define(function() {
     console.log('\x1b[31m', `✘ ${details.message}` +
       ` -- Expected: ${details.expected}, actual: ${details.actual}.`);
   });
+  //
+  // __createVisualTest__ returns a configured test function generator. Tests
+  // should be invoked tests on document ready. Configuration:
+  //
+  // - `label` - header text describing the test topic.
+  // - `anchorName` - for the section on its docs page, used to generate `docsUrl`.
+  // - `asFragments` - will render the test differently, directly inside `<body>`.
+  // - `className` - a hook for any custom css.
+  // - `footerHtml` - optional html for controls for additional testing.
+  //
+  // It will:
+  // 1. Ensure config.
+  // 2. Build test template vars.
+  // 3. Render test to get test context (or fragments).
+  // 4. Run tests.
+  //
+  // It also includes helpers for additional setup. `setupAppendButton` inits a
+  // button for appending an item to a list, and should be included as part of
+  // `footerHtml`.
+  //
+  function createVisualTest(config) {
+    return function() {
+      const {
+        asFragments, anchorName, beforeTest, className, label, template, test
+      } = config;
+      let { footerHtml, vars } = config;
+      if (!asFragments) {
+        footerHtml = footerHtml || '';
+      }
+      vars = vars || {};
+      let docsUrl = document.querySelector('body > header [data-rel=docs]').href;
+      docsUrl += `#${anchorName}`;
+      let html = template(vars);
+      let containerElement = asFragments ? document.body : document.getElementById('main');
+      let opts;
+      if (asFragments) {
+        opts = { template: (vars => `${vars.html}`) };
+      }
+      let testElement = renderVisualTest(containerElement,
+        { className, docsUrl, footerHtml, html, label }, opts
+      );
+      if (asFragments) {
+        if (className) {
+          containerElement.classList.add(className);
+        }
+        testElement.classList.add('visual-test-fragment');
+      }
+      if (beforeTest) {
+        beforeTest(testElement);
+      }
+      test(testElement);
+    };
+  }
+  Object.assign(createVisualTest, {
+    setupAppendButton(testElement, listSelector, updateItem) {
+      let listElement = testElement.querySelector(listSelector);
+      let itemElement = listElement.lastChild.cloneNode();
+      testElement.querySelector('[name=list-append]').addEventListener('click', () => {
+        let newElement = itemElement.cloneNode();
+        listElement.appendChild(newElement);
+        if (updateItem) {
+          updateItem(newElement);
+        }
+      });
+    }
+  });
+  //
+  // __renderVisualTest__ is a simple method for an element to render and
+  // append a test. Only provide a custom template if absolutely needed.
+  //
+  function renderVisualTest(containerElement, vars, opts) {
+    var $test;
+    if (opts == null) {
+      opts = {};
+    }
+    let { template } = Object.assign({}, renderVisualTest.defaults, opts);
+    let templateElement = document.createElement('template');
+    templateElement.innerHTML = template(vars);
+    let testElement = template.content.firstChild;
+    containerElement.appendChild(testElement);
+    return testElement;
+  }
+  renderVisualTest.defaults = {
+    template: (vars) => (
+`<section class="${vars.className} visual-test">
+  <header>
+    <span class="label">${vars.label}</span>
+    <a data-rel="docs" href="${vars.docsUrl}">docs</a>
+  </header>
+  ${vars.html}
+  <footer>${vars.footerHtml}</footer>
+</section>`
+    )
+  };
+  const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed " +
+    "do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+    "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
+    "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " +
+    "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
+    "est laborum.";
+  const placeholderText = {
+    long: loremIpsum,
+    short: loremIpsum.slice(0, 200),
+    title: loremIpsum.slice(0, 26)
+  };
+  Object.assign(namespace, { createVisualTest, placeholderText, renderVisualTest });
 
   return namespace;
 });
