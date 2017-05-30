e80675e481ed0dd4de4b78cfcf9e7f77e12e8776
Peng Wang <peng@pengxwang.com>
2017-05-29 19:28:54 -0700

    ES6 rewrite: continue porting build file, inline.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,35 @@
 const matchdep = require('matchdep');
 
 let aspects = {}; // Like an aspect of work. Somewhat maps to directories and tasks.
-['dist', 'docs', 'gh-pages', 'lib', 'release', 'src', 'tests']
+['docs', 'gh-pages', 'lib', 'release', 'src', 'tests']
   .forEach(name => aspects[name] = require(`./build/${name}`));
+
+const dist = {
+  clean: [
+    'dist/*',
+    '!dist/.gitignore',
+  ],
+  // # Copy original css for importing.
+  copy: {
+    expand: true,
+    src: 'src/**/*.css',
+    dest: 'dist/',
+    extDot: 'last',
+    flatten: true,
+  },
+  registerTasks(grunt) {
+    grunt.registerTask('dist', ['clean:dist', 'copy:dist', 'coffee:src']);
+    // This task's optimized for speed, at cost of artifacts.
+    grunt.registerTask('lazy-dist', ['newer:copy:dist', 'newer:coffee:src']);
+  },
+};
 
 module.exports = (grunt) => {
   config = {
     pkg: grunt.file.readJSON('package.json'),
     bump: aspects.release.bump,
     clean: {
-      dist: aspects.dist.clean,
+      dist: dist.clean,
       docs: aspects.docs.clean,
       'gh-pages': aspects['gh-pages'].clean,
       lib: aspects.lib.clean,
@@ -20,7 +40,7 @@ module.exports = (grunt) => {
       tests: aspects.tests.coffee,
     },
     copy: {
-      dist: aspects.dist.copy,
+      dist: dist.copy,
       'gh-pages': aspects['gh-pages'].copy,
       lib: aspects.lib.copy,
       release: aspects.release.copy,
@@ -55,6 +75,7 @@ module.exports = (grunt) => {
   grunt.registerTask('default', ['lazy-dist', 'watch']);
   grunt.registerTask('install', ['lib', 'dist']);
 
+  dist.registerTasks(grunt);
   for (const name in aspects) {
     if (aspects.hasOwnProperty(name)) {
       if (name === 'src') { continue; }
