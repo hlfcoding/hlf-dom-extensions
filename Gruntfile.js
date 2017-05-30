c2efd8ca62cfb968dbc10196032525686b4a7222
Peng Wang <peng@pengxwang.com>
2017-05-29 19:28:50 -0700

    ES6 rewrite: start porting build file.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+const matchdep = require('matchdep');
+
+let aspects = {}; // Like an aspect of work. Somewhat maps to directories and tasks.
+['dist', 'docs', 'gh-pages', 'lib', 'release', 'src', 'tests']
+  .forEach(name => aspects[name] = require(`./build/${name}`));
+
+module.exports = (grunt) => {
+  config = {
+    pkg: grunt.file.readJSON('package.json'),
+    bump: aspects.release.bump,
+    clean: {
+      dist: aspects.dist.clean,
+      docs: aspects.docs.clean,
+      'gh-pages': aspects['gh-pages'].clean,
+      lib: aspects.lib.clean,
+      release: aspects.release.clean,
+    },
+    coffee: {
+      src: aspects.src.coffee,
+      tests: aspects.tests.coffee,
+    },
+    copy: {
+      dist: aspects.dist.copy,
+      'gh-pages': aspects['gh-pages'].copy,
+      lib: aspects.lib.copy,
+      release: aspects.release.copy,
+    },
+    'gh-pages': {
+      'gh-pages': aspects['gh-pages']['gh-pages'],
+    },
+    groc: {
+      docs: aspects.docs.groc,
+    },
+    markdown: {
+      'gh-pages': aspects['gh-pages'].markdown,
+    },
+    qunit: {
+      tests: aspects.tests.qunit,
+    },
+    uglify: {
+      release: aspects.release.uglify,
+    },
+    watch: {
+      // Caveat: These watch tasks do not clean.
+      js: aspects.src.watch.js,
+    },
+  };
+  if (!grunt.option('fast')) {
+    config.watch.docs = aspects.docs.watch;
+  }
+  grunt.initConfig(config);
+
+  matchdep.filterDev('grunt-*').forEach(plugin => grunt.loadNpmTasks(plugin));
+
+  grunt.registerTask('default', ['lazy-dist', 'watch']);
+  grunt.registerTask('install', ['lib', 'dist']);
+
+  for (const name in aspects) {
+    if (aspects.hasOwnProperty(name)) {
+      if (name === 'src') { continue; }
+      aspects[name].task();
+    }
+  }
+};
