361a6d1e8cf743c3d65c6954cc38a4fb8e58fbcb
Peng Wang <peng@pengxwang.com>
2017-06-19 22:45:25 -0700

    Minor media-grid visual test refactor.

--- tests/js/media-grid.js
+++ tests/js/media-grid.js
@@ -70,6 +70,15 @@
       label: 'by default',
       template(vars) {
         const { placeholderText } = vars;
+        const tagsHTML = (
+`
+<ul class="mg-tags">
+  <li>foo</li>
+  <li>bar</li>
+  <li>baz</li>
+</ul>
+`
+        );
         let html = '<div class="test-body mg-theme-folio">';
         let i = 12;
         do {
@@ -81,11 +90,7 @@
       <h1 class="mg-title">${placeholderText.title}</h1>
       <h2 class="mg-date">July 14, 2012</h2>
     </div>
-    <ul class="mg-tags">
-      <li>foo</li>
-      <li>bar</li>
-      <li>baz</li>
-    </ul>
+    ${tagsHTML}
   </section>
   <section class="mg-detail">
     <img src="http://placehold.it/418x280/888/aaa" alt="main banner" />
@@ -94,11 +99,7 @@
       <h2 class="mg-date">July 14, 2012</h2>
     </div>
     <p>${placeholderText.short}</p>
-    <ul class="mg-tags">
-      <li>foo</li>
-      <li>bar</li>
-      <li>baz</li>
-    </ul>
+    ${tagsHTML}
   </section>
 </article>`
           );
