6c3b7d365719ec827a11865eed6546de2451f366
Peng Wang <peng@pengxwang.com>
2017-07-16 18:42:57 -0700

    Continue tip port: main update methods.

--- src/js/tip.js
+++ src/js/tip.js
@@ -47,10 +47,54 @@
   };
   class Tip {
     constructor(elements, options, contextElement) {
+      this._state = {
+        name: null,
+        sleepCountdown: null,
+        wakeCountdown: null,
+      };
     }
     init() {
+      this.element = document.createElement('div');
+      this._updateState('asleep');
+      this._currentTriggerElement = null;
     }
     deinit() {
+    }
+    _dispatchStateEvent() {
+      if (!this.doDispatchEvents) { return; }
+      let triggerElement = this._currentTriggerElement;
+      if (!triggerElement) { return; }
+      let eventName = (() => {
+        switch (this._state.name) {
+          case 'asleep': return 'hidden';
+          case 'awake': return 'shown';
+          case 'sleeping': return 'hide';
+          case 'waking': return 'show';
+        }
+      }());
+      triggerElement.dispatchEvent(this.createCustomEvent(eventName));
+    }
+    _toggleElementPositionTransition(toggled) {
+      this.element.style.transition = toggled ? 'transform 0.1s linear' : '';
+    }
+    _updateState(name) {
+      let state = this._state;
+      if (name === state.name) { return; }
+      state.name = name;
+      this.debugLog(name);
+
+      this._dispatchStateEvent();
+      if (state.name === 'asleep' || state.name === 'awake') {
+        setTimeout(() => {
+          this._toggleElementPositionTransition(false);
+        }, 0);
+      } else if (state.name === 'sleeping') {
+        clearTimeout(state.wakeCountdown);
+        state.wakeCountdown = null;
+      } else if (state.name === 'waking') {
+        clearTimeout(state.sleepCountdown);
+        state.sleepCountdown = null;
+      }
     }
   }
   return hlf.createExtension({
