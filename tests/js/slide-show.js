c41fbaac4592ece74a7e3d9b48d5c8d2caa4cfbe
Peng Wang <peng@pengxwang.com>
2017-12-31 13:29:27 -0800

    SlideShow: add initial visual test to expose flaws.

---
+++ tests/js/slide-show.js
@@ -0,0 +1,44 @@
+//
+// HLF SlideShow Visual Tests
+// ==========================
+// [Page](../../../tests/slide-show.visual.html) | [Source](../../src/js/slide-show.html)
+//
+(function() {
+  'use strict';
+  if (window.guard && !guard.isNavigatorSupported) { return; }
+
+  require.config({ baseUrl: '../', paths: { hlf: 'dist', test: 'tests/js' } });
+  define(['test/base', 'hlf/slide-show'], function(base, SlideShow) {
+    let tests = [];
+    const { createVisualTest, placeholderText, runVisualTests } = base;
+    tests.push(createVisualTest({
+      label: 'by default',
+      template({ placeholderText, slideCount }) {
+        let slidesHtml = '';
+        [...Array(slideCount)].forEach((_, i) => {
+          slidesHtml += (
+`<li class="slide">
+  <figure>
+    <img src="resources/slide.png">
+  </figure>
+</li>`
+          );
+        });
+        return (
+`<div class="slideshow">
+  <ol class="slides">
+    ${slidesHtml}
+  </ol>
+</div>`
+        );
+      },
+      test(testElement) {
+        SlideShow.extend(testElement.querySelector('.slideshow'));
+      },
+      anchorName: 'default',
+      className: 'default-call',
+      vars: { placeholderText, slideCount: 3 },
+    }));
+    runVisualTests(tests);
+  });
+}());
